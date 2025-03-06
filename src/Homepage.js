859650dc79693c2909b6e3274689a07399cc4284
BellaAkins <sarabellaakins@gmail.com>
2025-03-06 23:32:00 +0100

    add content

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,12 +1,14 @@
 const Homepage = () => {
   return (
     <div className="homepage">
-      <h1>Welcome to my Blog</h1>
-      <p>
-        Here you can find all the latest and greatest blog posts on the
-        internet. Whether you are interested in technology, sports, or
-        entertainment, we have something for you.
-      </p>
+      {<h1>Welcome to my Blog</h1>}
+      {
+        <p>
+          Here you can find all the latest and greatest blog posts on the
+          internet. Whether you are interested in technology, sports, or
+          entertainment, we have something for you.
+        </p>
+      }
     </div>
   );
 };

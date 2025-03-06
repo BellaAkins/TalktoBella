a270838e162a2861ee0716a83a3d6d898f3c4bb2
BellaAkins <sarabellaakins@gmail.com>
2025-03-06 23:36:43 +0100

    wo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import Homepage from "./Homepage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homepage />
     </div>
   );
 }

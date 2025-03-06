b179c73a7b62b58769275a4a78e2f7ff58435444
BellaAkins <sarabellaakins@gmail.com>
2025-03-06 22:25:09 +0100

    add coontent

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,7 +1,12 @@
 const Homepage = () => {
   return (
     <div className="homepage">
-      <h1>Welcome to TalktoBella Blog</h1>
+      <h1>Welcome to my Blog</h1>
+      <p>
+        Here you can find all the latest and greatest blog posts on the
+        internet. Whether you are interested in technology, sports, or
+        entertainment, we have something for you.
+      </p>
     </div>
   );
 };

3aaca8c1aab795ed6a89bd4f2c23734a9ac94a43
BellaAkins <sarabellaakins@gmail.com>
2025-03-06 02:31:10 +0100

    add links

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,10 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <h1>TalktoBella</h1>
+      <div className="links">
+        {<a href="/">Home</a>}
+        {<a href="/">New Blog</a>}
+      </div>
     </div>
   );
 };

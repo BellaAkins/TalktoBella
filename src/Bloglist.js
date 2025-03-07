ebcf129b09d8764ecd86b016c8efc7ec43ca10c3
BellaAkins <sarabellaakins@gmail.com>
2025-03-07 03:05:09 +0100

    Update Bloglist.js, Homepage.js and add NotFound.js

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,5 +1,18 @@
-const BlogList = () => {
-  return <div className="blog-list"></div>;
+const BlogList = ({ blogs, title, handleDelete }) => {
+  return (
+    <div className="bloglist">
+      <h2>{title}</h2>
+      {blogs.map((singleBlog) => (
+        <div className="blog-preview" key={singleBlog.id}>
+          <h2>{singleBlog.title}</h2>
+          <p>A Blog by {singleBlog.author}</p>
+          <button onClick={() => handleDelete(singleBlog.id)}>
+            Delete Blog
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BlogList;
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,14 +1,59 @@
+import { useEffect, useState } from "react";
+import BlogList from "./Bloglist";
+
 const Homepage = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "Welcome to my new website. I hope you enjoy your stay!",
+      author: "Bella",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "Join us this Saturday for a welcome party at my house!",
+      author: "TioluwaLope",
+      id: 2,
+    },
+    {
+      title: "The best burger in town",
+      body: "Come try the best burger in town at Joe's Burgers!",
+      author: "TioluwaLope",
+      id: 3,
+    },
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((singleBlog) => singleBlog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("it worked");
+  });
   return (
-    <div className="homepage">
-      {<h1>Welcome to my Blog</h1>}
-      {
-        <p>
-          Here you can find all the latest and greatest blog posts on the
-          internet. Whether you are interested in technology, sports, or
-          entertainment, we have something for you.
-        </p>
-      }
+    <div className="homepage ">
+      <div className="blogheader">
+        {<h1>Welcome to my Blog</h1>}
+        {
+          <p>
+            Here you can find all the latest and greatest blog posts on the
+            internet. Whether you are interested in technology, sports, or
+            entertainment, we have something for you. you can also add your own
+            blog post by clicking the button below.
+          </p>
+        }
+      </div>
+
+      <div className="blog-preview">
+        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+        {/* <BlogList
+          blogs={blogs.filter(
+            (singleBlog) => singleBlog.author === "TioluWaLope"
+          )}
+          title="Bella Blogs"
+        /> */}
+      </div>
     </div>
   );
 };
